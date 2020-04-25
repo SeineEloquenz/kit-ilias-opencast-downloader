81b014642bf5d3097ee52793a6858e0189bc3f13
Alexander Linder <alexander-linder@gmx.de>
2020-04-25 23:50:18 +0200

    Add licensing information

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Kit Ilias OpenCast Downloader
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
 
 This chrome extension adds a download button to lecture recordings in the KIT ilias system.
 The extension downloads them by finding the link to the video file in the page source.
--- background.js
+++ background.js
@@ -1,4 +1,21 @@
 
+/**
+ KIT Ilias OpenCast Downloader - a browser extension to simplify downloading videos from the KIT Ilias system
+ Copyright (C) 2020-present Alexander Linder
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see {http://www.gnu.org/licenses/}.
+
+ Home: https://github.com/SeineEloquenz/kit-ilias-opencast-downloader
+ */
+
 let urls = new Map();
 
 //Remove download urls when tabs are closed
